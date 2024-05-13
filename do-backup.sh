b4b7ba268f216f344512892849c35b14743c5a37
Jacob Sawatzky <jsawatzky@outlook.com>
2024-05-13 18:48:14 -0400

    Support Healthchecks.io pings

--- do-backup.sh
+++ do-backup.sh
@@ -13,12 +13,25 @@ backup() {
     echo "$(date +%y-%m-%dT%H:%M:%S) [INFO] Backing up $1..."
     if restic backup $DRYRUN_ARG -H $HOSTNAME --exclude-caches --one-file-system $exclude_arg $1; then
         echo "$(date +%y-%m-%dT%H:%M:%S) [INFO] Successfully backed up $1"
+        return 0
     else
         echo "$(date +%y-%m-%dT%H:%M:%S) [ERROR] Failed to backup $1"
+        return 1
     fi
 }
 
+if [ ! -z $HEALTHCHECKS_URL ]; then
+    finish() {
+        curl -m 10 --retry 5 $HEALTHCHECKS_URL/$?
+    }
+    trap finish EXIT
+
+    curl -m 10 --retry 5 $HEALTHCHECKS_URL/start
+fi
+
+exit_code=0
 DIRS="/mnt/*"
 for d in $DIRS; do
-    backup $d
+    backup $d || exit_code=1
 done
+exit $exit_code
