11989086e8faabd6b49668bef84ad275423c6c5e
Jacob Sawatzky <jsawatzky@outlook.com>
2024-05-16 16:56:00 -0400

    Handle paths with spaces

--- do-backup.sh
+++ do-backup.sh
@@ -6,12 +6,12 @@ fi
 
 backup() {
     exclude_arg=""
-    if [ -f $1/.restic-exclude ]; then
-        exclude_arg="--exclude-file $1/.restic-exclude"
+    if [ -f "$1/.restic-exclude" ]; then
+        exclude_arg="--exclude-file \"$1/.restic-exclude\""
     fi
 
     echo "$(date +%y-%m-%dT%H:%M:%S) [INFO] Backing up $1..."
-    if restic backup $DRYRUN_ARG -H $HOSTNAME --exclude-caches --one-file-system $exclude_arg $1; then
+    if restic backup $DRYRUN_ARG -H $HOSTNAME --exclude-caches --one-file-system $exclude_arg "$1"; then
         echo "$(date +%y-%m-%dT%H:%M:%S) [INFO] Successfully backed up $1"
         return 0
     else
@@ -32,6 +32,6 @@ fi
 exit_code=0
 DIRS="/mnt/*"
 for d in $DIRS; do
-    backup $d || exit_code=1
+    backup "$d" || exit_code=1
 done
 exit $exit_code
